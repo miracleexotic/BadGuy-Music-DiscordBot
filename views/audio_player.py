d78055703368286f9a8038405868a369bd967761
miracleexotic <mai_nutthawat@hotmail.com>
2025-05-14 01:37:58 +0700

    Change: audio player view - responding to the interaction first

--- views/audio_player.py
+++ views/audio_player.py
@@ -1,38 +1,47 @@
 import discord
 from discord.ext import commands
 
-from controllers import (
-    ytdl,
-    voice
-)
+from controllers import ytdl, voice
+
 
 class SongReqModal(discord.ui.Modal):
 
     name = discord.ui.TextInput(
-        label='Song name?',
-        placeholder='Song name here...',
+        label="Song name?",
+        placeholder="Song name here...",
     )
 
-    def __init__(self, *, title: str = 'Music Request', bot: commands.Bot, ctx: commands.Context, view: discord.ui.View) -> None:
+    def __init__(
+        self,
+        *,
+        title: str = "Music Request",
+        bot: commands.Bot,
+        ctx: commands.Context,
+        view: discord.ui.View
+    ) -> None:
         super().__init__(title=title, timeout=None)
         self.bot = bot
         self.ctx = ctx
         self.view = view
 
     async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.edit_message(view=self.view)
         async with self.ctx.typing():
             try:
-                source = await ytdl.YTDLSource.create_source(self.ctx, self.name.value, loop=self.bot.loop)
+                source = await ytdl.YTDLSource.create_source(
+                    self.ctx, self.name.value, loop=self.bot.loop
+                )
             except ytdl.YTDLError as e:
-                await self.ctx.send('An error occurred while processing this request: {}'.format(str(e)))
+                await self.ctx.send(
+                    "An error occurred while processing this request: {}".format(str(e))
+                )
             else:
                 if not self.ctx.voice_state.voice:
                     await self.ctx.invoke(self._join)
 
                 song = voice.Song(source)
                 await self.ctx.voice_state.songs.put(song)
-                await self.ctx.send('Enqueued {}'.format(str(source)))
-        await interaction.response.edit_message(view=self.view)
+                await self.ctx.send("Enqueued {}".format(str(source)))
 
     # async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
     #     await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
@@ -51,12 +60,18 @@ class AudioPlayerView(discord.ui.View):
     @discord.ui.button(label="Pause", style=discord.ButtonStyle.red)
     async def play(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.start:
-            if self.ctx.voice_state.is_playing and self.ctx.voice_state.voice.is_playing():
+            if (
+                self.ctx.voice_state.is_playing
+                and self.ctx.voice_state.voice.is_playing()
+            ):
                 self.ctx.voice_state.voice.pause()
             button.label = "Play"
             button.style = discord.ButtonStyle.green
         else:
-            if self.ctx.voice_state.is_playing and self.ctx.voice_state.voice.is_paused():
+            if (
+                self.ctx.voice_state.is_playing
+                and self.ctx.voice_state.voice.is_paused()
+            ):
                 self.ctx.voice_state.voice.resume()
             button.label = "Pause"
             button.style = discord.ButtonStyle.red
@@ -65,10 +80,12 @@ class AudioPlayerView(discord.ui.View):
 
     # Next
     @discord.ui.button(label="Next", style=discord.ButtonStyle.blurple)
-    async def next_song(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def next_song(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
         voter = self.ctx.message.author
         if voter == self.ctx.voice_state.current.requester:
-            await self.ctx.message.add_reaction('⏭')
+            await self.ctx.message.add_reaction("⏭")
             self.ctx.voice_state.skip()
 
         elif voter.id not in self.ctx.voice_state.skip_votes:
@@ -76,13 +93,15 @@ class AudioPlayerView(discord.ui.View):
             total_votes = len(self.ctx.voice_state.skip_votes)
 
             if total_votes >= 3:
-                await self.ctx.message.add_reaction('⏭')
+                await self.ctx.message.add_reaction("⏭")
                 self.ctx.voice_state.skip()
             else:
-                await self.ctx.send('Skip vote added, currently at **{}/3**'.format(total_votes))
+                await self.ctx.send(
+                    "Skip vote added, currently at **{}/3**".format(total_votes)
+                )
 
         else:
-            await self.ctx.send('You have already voted to skip this song.')
+            await self.ctx.send("You have already voted to skip this song.")
         await interaction.response.edit_message(view=self)
 
     # Loop
@@ -98,5 +117,7 @@ class AudioPlayerView(discord.ui.View):
 
     # Add
     @discord.ui.button(label="+ Add", style=discord.ButtonStyle.grey)
-    async def songreq(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def songreq(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
         await interaction.response.send_modal(self.song_req_modal)
