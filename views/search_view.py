29f158d73ecba696191cc2c4a6d4cf55bb28c216
miracleexotic <mai_nutthawat@hotmail.com>
2025-05-18 11:07:13 +0700

    Edit: views/search_view - add defer

--- views/search_view.py
+++ views/search_view.py
@@ -3,40 +3,56 @@ from discord.ext import commands
 
 BUTTON_CLR = discord.ButtonStyle.gray
 
+
 class SearchView(discord.ui.View):
     def __init__(self, bot: commands.Bot, ctx: commands.Context):
         super().__init__(timeout=45.0)
         self.bot = bot
         self.ctx = ctx
         self.value = None
-    
+
     # 1
     @discord.ui.button(label="1", style=BUTTON_CLR)
-    async def _song_1(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def _song_1(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        await interaction.response.defer()
         self.value = "1"
         self.stop()
 
     # 2
     @discord.ui.button(label="2", style=BUTTON_CLR)
-    async def _song_2(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def _song_2(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        await interaction.response.defer()
         self.value = "2"
         self.stop()
 
     # 3
     @discord.ui.button(label="3", style=BUTTON_CLR)
-    async def _song_3(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def _song_3(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        await interaction.response.defer()
         self.value = "3"
         self.stop()
 
     # 4
     @discord.ui.button(label="4", style=BUTTON_CLR)
-    async def _song_4(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def _song_4(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        await interaction.response.defer()
         self.value = "4"
         self.stop()
 
     # 5
     @discord.ui.button(label="5", style=BUTTON_CLR)
-    async def _song_5(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def _song_5(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        await interaction.response.defer()
         self.value = "5"
         self.stop()
 
@@ -51,7 +67,7 @@ class SearchView(discord.ui.View):
     # async def _song_7(self, interaction: discord.Interaction, button: discord.ui.Button):
     #     self.value = "7"
     #     self.stop()
-    
+
     # # 8
     # @discord.ui.button(label="8", style=discord.ButtonStyle.blurple)
     # async def _song_8(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -72,8 +88,8 @@ class SearchView(discord.ui.View):
 
     # Cancel
     @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
-    async def _cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
-        self.value = 'cancel'
+    async def _cancel(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        self.value = "cancel"
         self.stop()
-
-    
